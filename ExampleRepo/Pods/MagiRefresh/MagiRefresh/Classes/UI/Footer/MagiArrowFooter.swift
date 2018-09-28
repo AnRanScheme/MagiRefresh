053b6ef2843e694eec97ecdca1bb37d991e74e32
AnRanScheme <541437674@qq.com>
2018-09-28 18:18:52 +0800

    save

--- ExampleRepo/Pods/MagiRefresh/MagiRefresh/Classes/UI/Footer/MagiArrowFooter.swift
+++ ExampleRepo/Pods/MagiRefresh/MagiRefresh/Classes/UI/Footer/MagiArrowFooter.swift
@@ -18,7 +18,8 @@ public class MagiArrowFooter: MagiRefreshFooterConrol {
     
     fileprivate lazy var arrowImgV: UIImageView = {
         let arrowImgV = UIImageView()
-        let path = Bundle.main.path(forResource: "Image", ofType: "bundle", inDirectory: nil) ?? ""
+        let bundle = Bundle(for: MagiArrowFooter.classForCoder())
+        let path = bundle.path(forResource: "Image", ofType: "bundle", inDirectory: nil) ?? ""
         let urlString = (path as NSString).appendingPathComponent("arrow.png")
         let image = UIImage(contentsOfFile: urlString)
         arrowImgV.image = image
