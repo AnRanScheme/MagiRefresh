40c4c1e38deed0ecd067991b47141f47c83d787d
AnRanScheme <541437674@qq.com>
2018-09-29 10:47:55 +0800

    修改资源文件路径 0.0.8

--- ExampleRepo/MagiRefresh/ViewController.swift
+++ ExampleRepo/MagiRefresh/ViewController.swift
@@ -18,6 +18,22 @@ func Delay(_ seconds: Double, completion:@escaping ()->()) {
 
 class ViewController: UIViewController {
     
+    fileprivate lazy var arrowImgV: UIImageView = {
+        let arrowImgV = UIImageView()
+        let curBundle = Bundle(for: MagiArrowFooter.classForCoder())
+        var curBundleDirectory = ""
+        if let curBundleName = curBundle.infoDictionary?["CFBundleName"] as? String {
+            curBundleDirectory = curBundleName+".bundle"
+        }
+        let path = curBundle.path(forResource: "Image", ofType: "bundle", inDirectory: curBundleDirectory) ?? ""
+        let urlString = (path as NSString).appendingPathComponent("arrow.png")
+        let image = UIImage(contentsOfFile: urlString)
+        arrowImgV.image = image
+        arrowImgV.layer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        
+        return arrowImgV
+    }()
+    
     lazy var tableView: UITableView = {
         let tableView = UITableView(frame: self.view.bounds,
                                     style: .plain)
@@ -26,7 +42,15 @@ class ViewController: UIViewController {
         tableView.dataSource = self
         tableView.register(UITableViewCell.self,
                            forCellReuseIdentifier: ViewController.identifier)
-        
+        let view = UIView(frame: CGRect(x: 0, y: 0,
+                                        width: self.view.bounds.width,
+                                        height: 44))
+        view.backgroundColor = UIColor.green
+        arrowImgV.frame = CGRect(x: 0, y: 0,
+                                 width: 20,
+                                 height: 20)
+        view.addSubview(arrowImgV)
+        tableView.tableHeaderView = view
         let header = MagiReplicatorHeader()
         header.themeColor = UIColor.red
         
@@ -53,6 +77,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+      
         view.addSubview(tableView)
     }
 
